893b41854a5eb1b7720a6ad921354a16a25924b6
antares68440 <30720551+antares68440@users.noreply.github.com>
2017-09-12 22:38:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+process.exit();
 const discord = require('discord.js');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
 const client = new discord.Client();
-var token = process.env.Discord_token
 const token = "MON TOKEN";
 
 //Lancement du bot
